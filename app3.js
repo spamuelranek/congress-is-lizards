92cd99c06c075e4d4d9d51b2c1161f9993f67c33
Samuel Panek <samuelrpanek@gmail.com>
2021-09-01 20:22:16 -0700

    added generator and buttons. Still working on coolness

--- app3.js
+++ app3.js
@@ -1,9 +1,9 @@
 function name_generate(){
-    var fname = document.getElementById("first_name");
+    var fname = document.getElementById("first_name").value;
 
-    var lname = document.getElementById("last_name");
+    var lname = document.getElementById("last_name").value;
 
-    var lizard = lname + "Lizard" + fname
+    var lizard = lname + " Lizard " + fname
 
     document.getElementById("result").innerHTML = lizard;
 }
---
+++ app4.js
@@ -0,0 +1,19 @@
+
+// fuction lcool(){
+//     var answer = document.getElementById("coolness").value;
+//         if(answer == 'yes'){
+//         document.getElementById("lizardAnswer").innerHTML = "SUPER COOL";
+// }
+
+// if(answer.toLocaleLowerCase() == 'no'){
+//     document.getElementById("lizardAnswer").innerHTML = "BOO";
+// }
+
+// }
+
+function lcool(){
+    var answer = document.getElementById("coolness").value;
+        if(answer == "yes"){
+            document.getElementById("lizardAnswer").innerHTML == "SUPER COOL";
+        }
+}
