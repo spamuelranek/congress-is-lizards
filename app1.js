aed68ec7dc20a634d4e83f8d832c105a1873fa0b
Samuel Panek <samuelrpanek@gmail.com>
2021-09-02 17:39:46 -0700

    added function tubleweed. get you some LawrenceBrock

--- app1.js
+++ app1.js
@@ -32,3 +32,48 @@ function lcool(){
             document.getElementById("lizardAnswer").innerHTML = "BOO"
         }
 }
+
+// function everyoneGW(){
+//     let userAnswer = prompt("How many pictures of XXXX would you like?");
+//         While(userAnswer !== 5){
+//             if(userAnswer ==5){
+//                 alert('You like them just the right amount');
+//                 let url ="president seal.jpg";
+//                 let picsofGW = 5
+//                 for(let i=0; i <= picsofGW; i++){
+//                     document.write('<img src="' + url + '">');
+//                 }
+//                 break;
+//             }
+//             else if(userAnswer < 5){
+//                 alert("Come on you like them more than that!")
+//             }
+//             if(userAnswer > 5){
+//                 alert("mmm...you might like them too much")
+//             } 
+//         }
+
+// }
+
+function tumbleweed(){
+    let userAnswer = prompt("How many pictures of Lawrence Brock (Nebraskan Representative from 1959-1961) would you like to see?");
+        while(userAnswer !== 5){
+            
+            if(userAnswer == 5){
+                    alert('You like them just the right amount');
+                    let url ="LawrenceBrock.jpg";
+                    let picsofGW = 4
+                    for(let i=0; i<=picsofGW; i++){
+                        document.write('<img src="' + url + '">');
+                    }
+                    break;
+            } else if(userAnswer < 5){
+                alert("Come on you like them more than that!");
+                userAnswer =prompt('How many pictures of Lawrence Brock (Nebraskan Representative from 1959-1961) would you like to see?');
+
+            } else if(userAnswer > 5){
+                alert("mmm...you might like them too much")
+                userAnswer =prompt('How many pictures of Lawrence Brock (Nebraskan Representative from 1959-1961) would you like to see?');
+            }
+        }
+}
